bc51850209c2540ed582a6f61179be7f9d8ce3b8
Роман Кашичкин <roman_kashichkin@mail.ru>
2023-07-21 00:57:39 +0300

    Sprint 16 (v4)

--- backend/recipes/management/commands/load_ingredient_csv.py
+++ backend/recipes/management/commands/load_ingredient_csv.py
@@ -1,6 +1,6 @@
 import csv
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from recipes.models import Ingredient
 
@@ -8,13 +8,29 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        file_name = 'recipes/data/ingredients.csv'
-        with open(file_name, 'r', encoding='utf-8') as file:
-            print(file)
-            file_reader = csv.reader(file)
-            for row in file_reader:
-                name, measurement_unit = row
-                Ingredient.objects.bulk_create(
-                    name=name,
-                    measurement_unit=measurement_unit
+        try:
+            file_name = 'recipes/data/ingredients.csv'
+            with open(file_name, 'r', encoding='utf-8') as file:
+                print(file)
+                file_reader = csv.reader(file, delimiter=',')
+                upload_list = []
+                for row in file_reader:
+                    name, measurement_unit = row
+                    new = Ingredient(
+                        name=name,
+                        measurement_unit=measurement_unit
+                    )
+                    if new not in upload_list:
+                        upload_list.append(new)
+                Ingredient.objects.bulk_create(upload_list)
+                self.stdout.write(
+                    self.style.SUCCESS('Ингредиенты успешно загружены')
                 )
+
+        except FileNotFoundError:
+            self.stdout.write(
+                self.style.ERROR('Ошибка')
+            )
+            raise CommandError(
+                'В директории recipes/data нет файла'
+            )
