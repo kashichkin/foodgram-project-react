fe4a0adb8a94f30026c85e550fdf781389a75890
Роман Кашичкин <roman_kashichkin@mail.ru>
2023-07-28 01:03:07 +0300

    Sprint 16 (v6)

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.72.135', 'backend']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost', 'backend').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
